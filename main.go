faac936ed1868b284cd24451ccf4c57b8892a00b
Khalid Alhazmi <kalid@Khalids-MacBook-Pro.local>
2024-02-22 17:23:28 +0300

    update main

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("hello world")
+	server := http.NewServeMux()
+	server.HandleFunc("/", basicHandler)
+	error := http.ListenAndServe(":3000", server)
+	if error != nil {
+		panic(error.Error())
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World\n"))
 }
